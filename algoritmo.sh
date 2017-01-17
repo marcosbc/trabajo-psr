86b6f62d8fe0d5518856e707407b09685da812de
Marcos Bjoerkelund <marcos@bitnami.com>
2017-01-17 11:42:13 +0100

    Finalizado algoritmo

--- algoritmo.sh
+++ algoritmo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-numNodos=2
-numNodosAnterior=1
-iterador=1
 
+# Ejemplo: 64kbps con 1Mbps
+ITERADOR_INI=2
+CENTRAL_KBPS=1000
+PROTOCOLO_KBPS=64
+MARGEN_ERROR=$((2**$ITERADOR_INI))
 
+numNodos=$((CENTRAL_KBPS/PROTOCOLO_KBPS))
+iterador=$ITERADOR_INI
+candidato=0
 
 function simulacion() {
   echo $1
@@ -11,25 +16,33 @@ function simulacion() {
 
 function cumple_con_requisitos() {
   echo "Comprobando $1..."
-  if [ "$1" -ge 129 ]; then
-    return 0
+  if [ "$1" -ge 199 ]; then
+    return 1
   fi
-  return 1
+  return 0
 }
 
-
-
-while [ $numNodosAnterior -ne $numNodos ]; do
+while [ $numNodos -ne $candidato ]; do
   resultados=$(simulacion $numNodos)
   cumple_con_requisitos $resultados
-  if [ $? -eq 1 ]; then
-    numNodosAnterior=$numNodos
+  if [ $? -eq 0 ]; then
+		candidato=$numNodos
     iterador=$((iterador+1))
   else
-    echo "NO CUMPLE"
-    numNodos=$numNodosAnterior
-    iterador=0
+    diferencia=$((numNodos-candidato))
+		if [ $numNodos -le $((candidato+MARGEN_ERROR)) ]; then
+			break
+		fi
+    numNodos=$candidato
+    iterador=$ITERADOR_INI
+    # Para buscar solucion exacta:
+		# if [ $candidato -eq $((numNodos-1)) ]; then
+		#   break
+		# fi
+    # numNodos=$candidato
+    # iterador=0
   fi
-  numNodos=$((numNodos+iterador*2))
-  echo "iterador=$iterador numNodosAnterior=$numNodosAnterior numNodos=$numNodos"
+  numNodos=$((numNodos+2**iterador))
 done
+
+echo "Encontrado optimo: $numNodos"
